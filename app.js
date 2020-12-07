3100afc5e1950aa4c068f3d01240f644583da769
James Torres <james.torres01@gmail.com>
2020-12-07 15:12:22 -0500

    cleaned up code WIP

--- app.js
+++ app.js
@@ -1,33 +1,31 @@
 $(() => {
-  $("form").on("submit", (event) => {
-
+  $("#button").on("click", (event) => {
       event.preventDefault();
 
-      const userInput = $('input[type = "text"]').val();
+      // const userInput = $('input[type = "text"]').val();
 
     $.ajax({
-      url: `https://pokeapi.co/api/v2/pokemon/${userInput.toLowerCase()}`
+      url: `https://pokeapi.co/api/v2/pokemon?limit=251`,
     }).then(
         (data) => {
-          console.log(data);
-        const $pokeBall = $('<div>').addClass('pokeBall');
-        $pokeBall.appendTo('.container');
+          // console.log(data);
+          for(let profile of data.results){
+            let pokeURL = profile.url;
 
-        const $pokeName = $('<h3>').text(data.name);
-        $pokeName.appendTo($pokeBall)
+            $.ajax({
+              url:pokeURL,
+            }).then((pokemon) => {
+              // console.log(pokemon);
 
-        const $index = $('<p>').text(data.id);
-        $index.appendTo($pokeBall);
-
-        const $image = $('<img>').attr('src' , `https://pokeres.bastionbot.org/images/pokemon/${userInput.toLowerCase()}.png`)
-        $image.appendTo($pokeBall)
-
-
-
-        // const $types = $('<ul>').addClass('types')
-        //   $types.appendTo($pokeBall).text(data.types)
-
+              let $pokeBall = $(`<div id="pokeBall">
+                <h3 id = "pokeName">Name:${profile.name}</h3>
+                <img src=https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png alt=${pokemon.name}>
+                <p id = "idex">Index:${pokemon.id}</p>`
+              ).appendTo(".container");
+            });
+          }
         },
+
         (error) => {
           console.log(error);
         }
@@ -47,3 +45,18 @@ $(() => {
 // $("#id").text(data.id);
 // $("#abilities").text(data.abilities[0].ability.name)
 // $("#description").text(data.types[0].type.name)
+// const $types = $('<ul>').addClass('types')
+//   $types.appendTo($pokeBall).text(types[0].type)
+
+//   console.log(data);
+// const $pokeBall = $('<div>').addClass('pokeBall');
+// $pokeBall.appendTo('.container');
+//
+// const $pokeName = $('<h3>').text(data.name);
+// $pokeName.appendTo($pokeBall)
+//
+// const $index = $('<p>').text(data.id);
+// $index.appendTo($pokeBall);
+//
+// const $image = $('<img>').attr('src' , `https://pokeres.bastionbot.org/images/pokemon/${userInput.toLowerCase()}.png`)
+// $image.appendTo($pokeBall)
